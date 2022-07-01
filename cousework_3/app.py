5f9cb076eb4f2ff30c2e1605c1e5a67da60dcca6
Dzhanet <104907844+Dzhanet@users.noreply.github.com>
2022-07-01 23:02:19 +0200

    Исправлено логирование, добавилось тестирование

--- cousework_3/app.py
+++ cousework_3/app.py
@@ -1,10 +1,13 @@
 from flask import Flask, render_template
 from bp_posts.views import posts_blueprint
 from bp_api.views import api_blueprint
+from logger import create_logger
 
 app = Flask(__name__)
 
 app.config['JSON_AS_ASCII'] = False
+
+create_logger()
 
 app.register_blueprint(posts_blueprint)
 app.register_blueprint(api_blueprint)
@@ -15,6 +18,13 @@ def pageNotFound(error):
 
     return render_template('error_404.html')
 
+
+@app.errorhandler(500)
+
+def pageServerError(error):
+    """Возвращает обратно, если ошибка 500"""
+
+    return render_template('error_404.html')
 
 
 if __name__ == "__main__":
--- cousework_3/tests/api_test.py
+++ cousework_3/tests/api_test.py
@@ -1,10 +1,21 @@
-import pytest
-from bp_api import views
+import random
+from app import app
+from utils import load_data_json
 
 
+class TestApi:
 
-post_keys = {'poster_name', 'poster_avatar', 'pic', 'content', 'views_count', 'likes_count', 'pk'}
+    def test_api_posts(self):
+        resp = app.test_client().get("/api/posts/", follow_redirects=True)
+        assert resp.status_code == 200
+        assert type(resp.json) == list
 
+    def test_api_post(self):
+        posts_data = load_data_json()
+        random_post = random.choice(posts_data)
+        resp = app.test_client().get(f"/api/posts/{random_post['pk']}", follow_redirects=True)
+        assert resp.status_code == 200
+        assert type(resp.json) == dict
+        post_keys = {'poster_name', 'poster_avatar', 'pic', 'content', 'views_count', 'likes_count', 'pk'}
+        assert resp.json.keys() == post_keys
 
-def test_posts_have_correct_keys(post):
-    pass
